a35556485d4b7426eafa8bf441e9f97821d63e3e
resikrys <resicris@gmail.com>
2025-06-01 23:56:47 +0200

    add presential worker class

---
+++ src/main/java/s1_07_annotations_lvl1/PresentialWorker.java
@@ -0,0 +1,19 @@
+package s1_07_annotations_lvl1;
+
+public class PresentialWorker extends Worker {
+    private static double gasoline = 50.0;
+
+    public PresentialWorker(String name, String surname, double hourPrice) {
+        super(name, surname, hourPrice);
+    }
+
+    @Override
+    public double salaryCalculator(int workedHours) {
+        return workedHours * hourPrice + gasoline;
+    }
+
+    @Deprecated
+    public void officeWork() {
+        System.out.println(name + " works from the office.");
+    }
+}
